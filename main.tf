4c7af34099aee7fc90fe9e26150e2cfe0af9426e
Pradeep Gandla <pradeepdoit@gmail.com>
2020-08-26 16:58:18 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_inst" {
+  ami = "ami-02354e95b39ca8dec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Web server"
+  }
+}
+
